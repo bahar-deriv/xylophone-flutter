8fbe61cdecbbdd87a289effe8245b573ae4f8add
Bahar <bahar@regentmarkets.com>
2023-05-26 16:16:03 +0800

    finalized

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,61 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({int soundNumber, Color color}) => Expanded(
+        child: TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(
+              color,
+            ),
+          ),
+          onPressed: () => playSound(soundNumber),
+          child: null,
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(
+                soundNumber: 1,
+                color: Colors.red,
+              ),
+              buildKey(
+                soundNumber: 2,
+                color: Colors.orange,
+              ),
+              buildKey(
+                soundNumber: 3,
+                color: Colors.yellow,
+              ),
+              buildKey(
+                soundNumber: 4,
+                color: Colors.green,
+              ),
+              buildKey(
+                soundNumber: 5,
+                color: Colors.teal,
+              ),
+              buildKey(
+                soundNumber: 6,
+                color: Colors.blue,
+              ),
+              buildKey(
+                soundNumber: 7,
+                color: Colors.purple,
+              ),
+            ],
           ),
         ),
       ),
